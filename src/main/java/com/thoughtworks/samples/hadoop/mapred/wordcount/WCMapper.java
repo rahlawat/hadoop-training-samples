c7230b33705264c3f392934f0a54b0b44c6b4eb6
rahlawat <renu.ahlawat21@gmail.com>
2014-01-17 10:03:34 +0530

    Undid the mess on wordcount Mapper.

--- src/main/java/com/thoughtworks/samples/hadoop/mapred/wordcount/WCMapper.java
+++ src/main/java/com/thoughtworks/samples/hadoop/mapred/wordcount/WCMapper.java
@@ -11,17 +11,18 @@ import java.util.regex.Pattern;
 public class WCMapper extends Mapper<Object, Text, Text, IntWritable> {
 
     private Pattern wordPattern
-            = Pattern.compile("[a-z0-9.-]+\\.[a-z]{2,4}");
+            = Pattern.compile("[^a-z0-9]*([a-z0-9]+)[^a-z0-9]*");
 
     @Override
     public void map(Object key, Text text, Context context)
             throws IOException, InterruptedException {
         String line = text.toString();
-        String[] tokens = line.split("@");
+        String[] tokens = line.split(" ");
         for (String token : tokens) {
-            Matcher wordMatcher = wordPattern.matcher(token);
+            Matcher wordMatcher = wordPattern.matcher(token.toLowerCase());
             if (wordMatcher.matches()) {
-                context.write(new Text(token), new IntWritable(1));
+                String word = wordMatcher.group(1);
+                context.write(new Text(word), new IntWritable(1));
             } else {
                 context.getCounter(SKIPPED_WORDS.MISMATCHED_WORD).increment(1);
             }
